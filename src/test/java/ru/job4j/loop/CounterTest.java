0fef5968063ad36da96c596218092089f788e17a
Vasilieva <masha.maria.vas@bk.ru>
2024-02-06 19:54:20 +0400

    5.1. Подсчет суммы чётных чисел в диапазоне [#192]

--- src/test/java/ru/job4j/loop/CounterTest.java
+++ src/test/java/ru/job4j/loop/CounterTest.java
@@ -7,7 +7,7 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 class CounterTest {
     @Test
-    void whenStartMinus3Finish7Then22() {
+    void whenStartMinus3Finish7ThenSum22() {
         int start = -3;
         int finish = 7;
         int expected = 22;
@@ -16,7 +16,7 @@ class CounterTest {
     }
 
     @Test
-    void whenStart47Finish2Then0() {
+    void whenStart47Finish2ThenSum0() {
         int start = 47;
         int finish = 2;
         int expected = 0;
@@ -25,11 +25,29 @@ class CounterTest {
     }
 
     @Test
-    void whenStartMinus6Finish0Then0() {
+    void whenStartMinus6Finish0ThenSum0() {
         int start = -6;
         int finish = 0;
         int expected = -21;
         int result = Counter.sum(start, finish);
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenSumEvenNumbersFromOneToTenThenThirty() {
+        int start = 1;
+        int finish = 10;
+        int result = Counter.sumByEven(start, finish);
+        int expected = 30;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenSumEvenNumbersFromMinus3ToFiveThenThirty() {
+        int start = -3;
+        int finish = 5;
+        int result = Counter.sumByEven(start, finish);
+        int expected = 4;
         assertThat(result).isEqualTo(expected);
     }
 }
