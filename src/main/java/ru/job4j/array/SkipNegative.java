d90a776eb4c7e5bb68c69541de0e22047de7a74d
Vasilieva <masha.maria.vas@bk.ru>
2024-02-16 18:42:59 +0400

    6.6.3. Двухмерный массив. If. [#306727 #481660]

--- src/main/java/ru/job4j/array/SkipNegative.java
+++ src/main/java/ru/job4j/array/SkipNegative.java
@@ -4,7 +4,9 @@ public class SkipNegative {
     public static int[][] skip(int[][] array) {
         for (int row = 0; row < array.length; row++) {
             for (int cell = 0; cell < array[row].length; cell++) {
-                array[row][cell] = array[row][cell] < 0 ? 0 : array[row][cell];
+                if (array[row][cell] < 0) {
+                    array[row][cell] = 0;
+                }
             }
         }
         return array;
