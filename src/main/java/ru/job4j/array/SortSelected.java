17bbf217b648b139a14d4f02470292aa990138e3
Vasilieva <masha.maria.vas@bk.ru>
2024-02-16 12:06:24 +0400

    6.5.3. Сортировка выборкой. [#156320]

--- src/main/java/ru/job4j/array/SortSelected.java
+++ src/main/java/ru/job4j/array/SortSelected.java
@@ -4,11 +4,12 @@ import ru.job4j.loop.SwitchArray;
 
 public class SortSelected {
     public static int[] sort(int[] data) {
+        int[] result = new int[data.length];
         for (int i = 0; i <= data.length - 1; i++) {
             int min = MinDiapason.findMin(data, i, data.length - 1);
             int index = FindLoop.indexInRange(data, min, i, data.length - 1);
-            data = SwitchArray.swap(data, index, i);
+            result = SwitchArray.swap(data, index, i);
         }
-        return data;
+        return result;
     }
 }
