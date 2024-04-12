7155d3f9bc9aa990b2af2d08c39207a065500e6a
Vasilieva <masha.maria.vas@bk.ru>
2024-04-12 21:33:21 +0400

    3. Массивы и модели. [#174892]

---
+++ src/main/java/ru/job4j/pojo/Library.java
@@ -0,0 +1,31 @@
+package ru.job4j.pojo;
+
+public class Library {
+    public static void main(String[] args) {
+        Book firstBook = new Book("Война и мир", 3645);
+        Book secondBook = new Book("Мастер и Маргарита", 2225);
+        Book thirdBook = new Book("Колобок", 50);
+        Book fourthBook = new Book("Clean code", 90);
+        Book[] books = new Book[4];
+        books[0] = firstBook;
+        books[1] = secondBook;
+        books[2] = thirdBook;
+        books[3] = fourthBook;
+        for (int i = 0; i < books.length; i++) {
+            System.out.println(books[i].getName() + " - " + books[i].getPages());
+        }
+        Book book = books[0];
+        books[0] = books[3];
+        books[3] = book;
+        System.out.println("Переставили местами книги с индексом 0 и 3");
+        for (int i = 0; i < books.length; i++) {
+            System.out.println(books[i].getName() + " - " + books[i].getPages());
+        }
+        System.out.println("Вывод книг с именем 'Clean code':");
+        for (int i = 0; i < books.length; i++) {
+            if (books[i].getName().equals("Clean code")) {
+                System.out.println(books[i].getName() + " - " + books[i].getPages());
+            }
+        }
+    }
+}
